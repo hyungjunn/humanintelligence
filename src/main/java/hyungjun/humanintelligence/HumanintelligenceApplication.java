030bb62f48f241cc1a781e30f2f2795c48565b37
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-07-15 06:59:52 +0900

    git init

---
+++ src/main/java/hyungjun/humanintelligence/HumanintelligenceApplication.java
@@ -0,0 +1,13 @@
+package hyungjun.humanintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanintelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanintelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hyungjun/humanintelligence/HumanintelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package hyungjun.humanintelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanintelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
